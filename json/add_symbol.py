cb84f293c6ab7874b507d00ae2c9922eda3e4f9b
cxumol <cxumol@163.com>
2018-06-23 02:59:03 +0000

    before replace seg_sym

--- json/add_symbol.py
+++ json/add_symbol.py
@@ -4,9 +4,33 @@ import re,os
 a = "seg"
 z = "seg_sym"
 
-for _,_,j in os.walk(a):
-    for i in j:
-        with open(a+'/'+i, 'r',encoding='utf8') as fa:
+for _,_,files in os.walk(a):
+    for afile in files:
+        with open(a+'/'+afile, 'r',encoding='utf8') as fa:
             a_content=fa.read()
-        all_NE = re.findall(r'(\[".*?", ".", "[BME]"\], )*', a_content)
-        print(i,len(all_NE))
+        all_NE = re.findall('''(?:\["[^"]+?", ".{1,4}", "[BME]"\], )+''', a_content)
+        print(afile)
+        all_NE=sorted(list(set(all_NE)))
+#         for x in all_NE:
+#             print(x)
+            
+        # 开始替换
+#         print("替换为: ")
+        ctrlfh= dict()
+        for each in all_NE:
+            ONE=re.sub('[BME]','.', each)
+            ONE=re.sub('\[','\[', ONE)
+            ONE=re.sub('\]','\]', ONE)
+            ctrlfh[ONE]=each
+#             print(ONE,':',ctrlfh[ONE])
+        
+        with open(z+'/seg_sym_'+afile[5:], 'w+',encoding='utf8') as fz:
+            z_content=fz.read()
+        
+            z_new_content=z_content
+            for k,v in ctrlfh.items():
+                z_new_content=re.sub(k,v, z_new_content)
+        
+            fz.write(z_new_content)
+#         with open(z+'/'+'_BME'+i, 'w',encoding='utf8') as fb:
+#             fb.write(z_new_content)
